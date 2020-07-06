6475585a3b6e92273c7719e15281ff194028f1fa
Sean Bordelon <sean.j.bordelon@gmail.com>
2020-07-06 17:45:14 -0400

    initial commit

---
+++ lib/Healthy_hunger_api.rb
@@ -0,0 +1,6 @@
+require "Healthy_hunger_api/version"
+
+module HealthyHungerApi
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/Healthy_hunger_api/version.rb
@@ -0,0 +1,3 @@
+module HealthyHungerApi
+  VERSION = "0.1.0"
+end
