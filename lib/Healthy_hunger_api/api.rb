deaf1aa3bf7079794562df5c22f1189f2e101499
Sean Bordelon <sean.j.bordelon@gmail.com>
2020-07-16 23:56:54 -0400

    fixing issue with api key

--- lib/Healthy_hunger_api/api.rb
+++ lib/Healthy_hunger_api/api.rb
@@ -8,6 +8,21 @@ module HealthyHungerApi
 
     class API #class that is accessing the api from the url
 
+      def self.api_key
+        begin 
+          @key = File.open(File.expand_path("~/.spoonacular-api-key")).read.strip
+        rescue
+          puts "Unable to locate your API key. Please go to rapidapit.com, search for the
+          recipe-food-nutrition api and subscribe to receive your api key. Then come back and
+          input your key here."
+          @key = gets.strip
+          File.open(File.expand_path("~/.spoonacular-api-key"), "W") do |file|
+            file.print @key
+          end
+        end
+        @key
+      end
+
       def self.get_meals (timeFrame, targetCalories, diet, exclude) #class method that is accepting input parameters to get specific info from url   
 
         url = URI("https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/mealplans/generate")
@@ -27,7 +42,7 @@ module HealthyHungerApi
 
         request = Net::HTTP::Get.new(url) #calling the Net::HTTP get method and passing the url to that method
         request["x-rapidapi-host"] = 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'
-        request["x-rapidapi-key"] = '3b2d9ac93amsh2ec6673c95ce5f3p174558jsna4a871c38f77'
+        request["x-rapidapi-key"] = api_key
 
         response = http.request(request)
         JSON.parse(response.read_body) if response.is_a?(Net::HTTPSuccess) #parsing through the return value of the response.body as long as the get request is successful
