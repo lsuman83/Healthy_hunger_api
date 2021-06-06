5ae6b409369883bcc2d61f06175dcf7911feaff4
lsuman83 <sean.j.bordelon@gmail.com>
2021-06-05 22:47:57 -0400

    reformatting json format within meals class

--- lib/Healthy_hunger_api/meals.rb
+++ lib/Healthy_hunger_api/meals.rb
@@ -38,54 +38,64 @@ module HealthyHungerApi
 
         end
 
-        attr_accessor :id, :title, :readyInMinutes, :servings, :sumnary, :day, :slot, :value #creates getter and setter methods for each
-                                                                                             #hash key received from the api meal hash               
+        attr_accessor  :id, :title, :readyInMinutes, :servings, :sumnary, :day, :slot, :value #creates getter and setter methods for each
+                                                                                     #hash key received from the api meal hash               
 
         def initialize(attributes = {}) #initializes new instances of each meal
             attributes.each do |attribute_name, attribute_value| #iterates over each has assigning each key as an attribute name
-                if self.respond_to?("#{attribute_name}=")       #and each key value as the attribute value using the send method
+                if (attribute_name == "value")
+                    initialize(JSON.parse(attribute_value))
+                elsif self.respond_to?("#{attribute_name}=")       #and each key value as the attribute value using the send method
                     self.send("#{attribute_name}=", attribute_value)
                 end
             end
         end
 
         
-        def value_parser #instance method used to parse through the class method get_summary from the API classes 
+       # def value_parser #instance method used to parse through the class method get_summary from the API classes 
                         #value key and grab the values that are within the "id" symbol
-            API.get_summary(JSON.parse(self.value)["id"])
+         #   API.get_summary(JSON.parse(self.value)["id"])
+           # Recipes.get_recipes(SON.parse(self.value)["id"]
 
-        end
+        #end
 
         
-        def daily_summaries #instance method that uses the id key from the return value of the class method
+        def summaries #instance method that uses the id key from the return value of the class method
                             #get_summary of the API class to get the summary of each meal for a daily meal plan
             
             @summaries ||= API.get_summary(self.id) 
             
         end
 
-        def weekly_summaries#instance method that calls the value_parser method to get the summary of each meal for a weekly meal plan
+        #def weekly_summaries#instance method that calls the value_parser method to get the summary of each meal for a weekly meal plan
 
-                @summaries ||= value_parser
+         #       @summaries ||= value_parser
 
-        end
+        #end
+
+       # def daily_recipes
+
+        #    @ = Recipes.get_recipes(self.id)
+
+        #end
 
 
-        def display_meals_by_day #instance method that uses the "summary" string to access the value from that hash symbol
+        def meals_by_day #instance method that uses the "summary" string to access the value from that hash symbol
                                 #and replace each of the symbols found through the regex passed into the .gsub method 
                                 #with an empty space
 
-            puts daily_summaries["summary"].gsub(/<\/?[a-z]+>/, "").gsub(/<a href="[a-zA-Z0-9\/:\.\-\+]+">/, "")   
+            puts summaries["summary"].gsub(/<\/?[a-z]+>/, "").gsub(/<a href="[a-zA-Z0-9\/:\.\-\+]+">/, "")   
+            
 
         end
 
-        def display_meals_by_week#instance method that uses the "summary" string to access the value from that hash symbol
+        #def display_meals_by_week#instance method that uses the "summary" string to access the value from that hash symbol
             #and replace each of the symbols found through the regex passed into the .gsub method 
             #with an empty space
 
-            puts weekly_summaries["summary"].gsub(/<\/?[a-z]+>/, "").gsub(/<a href="[a-zA-Z0-9\/:\.\-\+]+">/, "")
+         #   puts weekly_summaries["summary"].gsub(/<\/?[a-z]+>/, "").gsub(/<a href="[a-zA-Z0-9\/:\.\-\+]+">/, "")
 
-        end
+        #end
 
     end
 
