1bb1d514736be5ebd880c06afe9fab512e20ebaa
lsuman83 <sean.j.bordelon@gmail.com>
2021-06-05 20:10:56 -0400

    gem package for API

---
+++ spec/Healthy_hunger_api_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe HealthyHungerApi do
+  it "has a version number" do
+    expect(HealthyHungerApi::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
